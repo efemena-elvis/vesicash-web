76ee6449c860070441c7ecfc6c5b73f3f13369d3
Rilwan <akinyusuf5@gmail.com>
2023-08-09 11:23:34 +0100

    added fb registration tracking

--- src/modules/auth/pages/register.vue
+++ src/modules/auth/pages/register.vue
@@ -282,6 +282,16 @@ export default {
           error: "Unable to create your account at this time",
         },
       });
+
+      const status = [200, 201].includes(response?.code);
+      const data = status ? response?.data : this.getRequestPayload;
+
+      window?.fbq("track", "CompleteRegistration", {
+        customer_name: `${data?.lastname} ${data?.firstname}`,
+        customer_email: data.email_address,
+        status,
+      });
+
       if (response.code === 201) {
         this.user_details = response.data;
         this.handleOTPInitiation(); // SEND USER OTP
