c3654dd76126361eb857c99dc188ceb7cb269a24
richy8 <menaelvisjones@gmail.com>
2023-10-27 10:51:59 +0100

    Modified api key introduction

--- src/modules/developer/store/mutations.js
+++ src/modules/developer/store/mutations.js
@@ -1,4 +1,19 @@
+import { constants } from "@/utilities";
+import { serviceUtils, serviceStorage } from "@/shared/services";
+
 export default {
   SET_API_KEYS: (state, keys) => (state.api_keys = keys),
-  SET_API_KEYS_DETAILS: (state, data) => (state.api_keys_details = data),
+
+  SET_API_KEYS_DETAILS: (state, data) => {
+    state.api_keys_details = data;
+
+    let encoded_public_key = serviceUtils.encodeString(data.public_key);
+    let encoded_private_key = serviceUtils.encodeString(data.private_key);
+
+    serviceStorage.setStorage({
+      storage_name: constants.VESICASH_USER_TOKEN,
+      storage_value: { 0: encoded_public_key, 1: encoded_private_key },
+      storage_type: "object",
+    });
+  },
 };
--- src/modules/merchant-of-records/modules/developer/store/getters.js
+++ src/modules/merchant-of-records/modules/developer/store/getters.js
@@ -2,4 +2,5 @@ export default {
   isPaymentInfoCompleted: (state) => state.info_entry_completed,
   getUserPaymentInfo: (state) => state.user_payment_info,
   getSelectedPaymentMethod: (state) => state.selected_payment_method,
+  getSelectedShippingType: (state) => state.selected_shipping_type,
 };
